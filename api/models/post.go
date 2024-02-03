b49f5055581275cac794a4a8394b7e434231630e
hagar <h.maghawry@awaed.co>
2024-02-03 21:57:54 +0200

    add form data to post model

--- api/models/post.go
+++ api/models/post.go
@@ -7,7 +7,8 @@ import (
 type Post struct {
 	ID        int       `gorm:"primaryKey;autoIncrement" json:"id"`          // dynamic primary key
 	UserID    uint      `gorm:"index" json:"user_id"`                        // foreign key to users table
-	Title     string    `gorm:"type:varchar(255)" json:"title"`              // string for title
-	Body      string    `gorm:"type:text" json:"body"`                       // string for body
+	Title     string    `json:"title" form:"title"`                          // string for title
+	Body      string    `json:"body" form:"body"`                            // string for body
 	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"` // timestamp of creation
+	// image     FileResponse `gorm:"image"`
 }
--- api/post/post_services.go
+++ api/post/post_services.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"com.test.users_api_test/api/models"
-	conventer "com.test.users_api_test/pkg/converter"
 	"com.test.users_api_test/pkg/validation"
 	"github.com/gin-gonic/gin"
 )
@@ -15,17 +14,18 @@ func CreatePostService(context *gin.Context) (*models.Post, error) {
 	// user check //
 	userId := context.Value("userId").(uint)
 
-	// post check //
-	post := models.Post{}
-	error := conventer.ParseRequestBody(context, &post)
-	if error != nil {
-		return nil, errors.New(fmt.Sprintf("error parsing JSON: %v", error))
+	// Validate input
+	input := models.Post{}
+	if err := context.ShouldBind(&input); err != nil {
+		return nil, errors.New(fmt.Sprintf("%s", err))
 	}
+
+	post := models.Post{Title: input.Title, Body: input.Body}
 
 	if isValid, errMessage := validation.IsValidCreatePost(post); !isValid {
 		return nil, fmt.Errorf(errMessage)
 	}
-
+	// Create post
 	createdPost, errr := CreatePostQuery(&post, userId)
 	if errr != nil {
 		return nil, errr
